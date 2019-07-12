360e56d6d5cefd7baca9f0dba288f84ffaa756d2
fazerog02 <fazerog02@gmail.com>
2019-07-12 20:15:34 +0900

    [add]サーバーからjsonを受け取ってブロックを生成する機能(未デバック)

--- Assets/VrScenes/Scripts/BlockManager.cs
+++ Assets/VrScenes/Scripts/BlockManager.cs
@@ -1,30 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 
 public class BlockManager : MonoBehaviour
 {
     private struct Block
     {
-        public Vector3 position;
+        public float x;
+        public float y;
+        public float z;
         public int ID;
+        public float time;
+        public bool put;
+        public int colorID;
 
-        public Block(float x, float y, float z, int id)
+        public Block(float x, float y, float z, int ID, float time, bool put, int colorID)
         {
-            position = new Vector3(x, y, z);
-            ID = id;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.ID = ID;
+            this.time = time;
+            this.put = put;
+            this.colorID = colorID;
+        }
+
+        public Vector3 getPosition()
+        {
+            Vector3 position = new Vector3(x, y, z);
+            return position;
         }
     }
 
-    private Block[] block_list = {
-        new Block(0.0f, 0.0f, 0.0f, 1),
-        new Block(3.0f, 0.0f, 3.3f, 2),
-        new Block(5.0f, 0.0f, 5.0f, 3),
-    };
-
     private void Start()
     {
-        placeBlock(block_list);
+        getDataFromServerAndCreateBlock();
     }
 
     void placeBlock(Block[] block_list)
@@ -32,7 +44,53 @@ public class BlockManager : MonoBehaviour
         Object cube = (GameObject)Resources.Load("Cube");
         for (int i = 0; i < block_list.Length; i++)
         {
-            Instantiate(cube, block_list[i].position, Quaternion.identity);
+            Instantiate(cube, block_list[i].getPosition(), Quaternion.identity);
         }
+    }
+
+    private Block[] jsonToBlock(string json)
+    {
+        // jsonの不要な文字列を削除
+        json.Trim('{', '}');
+        json = json.Replace("[", "");
+        json = json.Replace("]", "");
+        json = json.Replace("blocks:", "");
+
+        string[] json_array = Regex.Split(json, @"},");  // 要素に分ける
+
+        // jsonからBlockを生成
+        List<Block> block_list = new List<Block>();
+        for (int i = 0; i < json_array.Length; i++)
+        {
+            json_array[i] = json_array[i].Insert(json_array[i].Length, "}");
+            Block block = JsonUtility.FromJson<Block>(json_array[i]);
+            block_list.Add(block);
+        }
+        Block[] blocks = block_list.ToArray();
+
+        return blocks;
+    }
+
+    async System.Threading.Tasks.Task getDataFromServerAndCreateBlock()
+    {
+        string server_url = "https://gulliverblocksserver.herokuapp.com";
+
+        string response_json;
+
+        using (var http_client = new HttpClient())
+        {
+            // getリクエストを投げる
+            HttpResponseMessage response = await http_client.GetAsync(server_url);
+
+            // 通信に失敗したら例外を投げる
+            response.EnsureSuccessStatusCode();
+
+            // レスポンスのbodyを読み込む
+            response_json = await response.Content.ReadAsStringAsync();
+        }
+
+        Block[] block_list = jsonToBlock(response_json);
+
+        placeBlock(block_list);
     }
 }

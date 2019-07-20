88784eabd423f9fda85c629f313c8c1c09a850d6
nPeeech <33167904+nPeeech@users.noreply.github.com>
2019-07-20 20:11:24 +0900

    Androidの戻るボタンでTitle Sceneに戻るようにした

---
+++ Assets/VrScenes/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+   public void Back_To_Title_If_Needed()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene("Title");
+            }
+        }
+    }
+}

b906ac42be265926e8a6d3e810f75ad89a6eff82
SoutaTanaka <tanakakesouta@yahoo.co.jp>
2019-08-05 09:15:30 +0900

    [fix] rotate camera by delta gyro position

--- Assets/VrScenes/Scripts/PlayerManager.cs
+++ Assets/VrScenes/Scripts/PlayerManager.cs
@@ -5,37 +5,55 @@ public class PlayerManager : MonoBehaviour
     [SerializeField] int default_move_speed = 1;
     [SerializeField] int run_move_speed = 2;
     Rigidbody player_rigidbody;
+    
     private bool isDefault_speed = true;
-    const string Stop = "Stop";
-    const string Forward = "Forward";
-    const string Right = "Right";
-    const string Left = "Left";
-    const string Back = "Back";
-    const string Up = "Up";
-    const string Down = "Down";
 
     public bool MoveRight, MoveLeft, MoveForward, MoveBack, MoveUp, MoveDown;
 
+/* 
+    public enum XAxisMove{
+        None,
+        Right,
+        Left
+    }
+    public enum YAxisMove{
+        None,
+        Up,
+        Down
+    }
+
+    public enum ZAxisMove{
+        None,
+        Forward,
+        Back
+    }
+    XAxisMove xMove = XAxisMove.None;
+    YAxisMove yMove = YAxisMove.None;
+    ZAxisMove zMove = ZAxisMove.None;
+    */
+
+#if UNITY_EDITOR
     private Vector3 lastMousePosition;
-    private Vector3 newAngle = new Vector3(0, 0, 0);
-    public Vector3 rotationSpeed;
-    #if UNITY_EDITOR
-    #else
-    public Touch touch = Input.GetTouch(0);
-    #endif
+#else
+    Quaternion lastGyro = Quaternion.identity;
+#endif
+    public Vector2 rotationSpeed = Vector2.one;
+    
 
     private void Awake()
     {
         player_rigidbody = GetComponent<Rigidbody>();
     }
+#if !UNITY_EDITOR
     void Start()
     {
-            Input.gyro.enabled = true;
+        Input.gyro.enabled = true;
+        lastGyro = Input.gyro.attitude;
     }
-
+#endif
     void Update ()
     {
-        Rotate();
+        RefleshRotation();
         Move();
     }
 
@@ -68,47 +86,48 @@ public class PlayerManager : MonoBehaviour
         else player_rigidbody.velocity = run_move_speed * moveDirection;
     }
 
-    public void Rotate()
+    void RefleshRotation()
     {
         #if UNITY_EDITOR //unityEditorでのデバッグ時
+        RefleshRotationByMouseDrag();
+        #else
+        RefleshRotationByGyro();
+        RefleshRotationByTouch();
+        #endif
+    }
+
+    #if UNITY_EDITOR
+
+    void RefleshRotationByMouseDrag(){
 
         if(Input.GetMouseButtonDown(0)){
             lastMousePosition = Input.mousePosition;
-            newAngle = transform.eulerAngles;
-
         }
-        else if(Input.GetMouseButton(0)){
-            newAngle.x -= (lastMousePosition.y - Input.mousePosition.y) * rotationSpeed.x;
-            newAngle.y -= (Input.mousePosition.x - lastMousePosition.x) * rotationSpeed.y;
-            //transform.eulerAngles += newAngle;
-            transform.rotation = Quaternion.Euler(newAngle);
-            lastMousePosition = Input.mousePosition;
+        else if (Input.GetMouseButton(0))
+        {
+            var currentMousePos = Input.mousePosition;
+            var deltaMousePos = currentMousePos - lastMousePosition;
+            transform.Rotate(-deltaMousePos.y * rotationSpeed.y,deltaMousePos.x * rotationSpeed.x,0);
+            lastMousePosition = currentMousePos;
         }
-
-        #else
-        if(Input.touchCount > 0){
-            touch = Input.GetTouch(0);
-        }
-
-        if(touch.phase == TouchPhase.Began){
-            lastMousePosition = Input.mousePosition;
-            newAngle = transform.eulerAngles;
-
-        }
-        else if(touch.phase == TouchPhase.Moved){
-            newAngle.x -= (lastMousePosition.y - Input.mousePosition.y) * rotationSpeed.x;
-            newAngle.y -= (Input.mousePosition.x - lastMousePosition.x) * rotationSpeed.y;
-            //transform.rotation = Quaternion.Euler(newAngle);
-            lastMousePosition = Input.mousePosition;
-        }
-        Quaternion gyro = Quaternion.AngleAxis(90.0f, Vector3.right) * Input.gyro.attitude * Quaternion.AngleAxis(180.0f, Vector3.forward);
-        transform.rotation = gyro ;//* Quaternion.Euler(newAngle);
-        transform.Rotate(newAngle);
-
-        #endif
 
     }
+    #else
+    void RefleshRotationByGyro(){
+        var currentGyro = Input.gyro.attitude;
+        var deltaRotation = currentGyro * Quaternion.Inverse(lastGyro);
+        transform.rotation *= Quaternion.Inverse(deltaRotation);
+        lastGyro = currentGyro;
+    }
 
+    void RefleshRotationByTouch(){
+        if (Input.touchCount > 0)
+        {
+            var deltaTouchPos = Input.GetTouch(0).deltaPosition;
+            transform.Rotate(-deltaTouchPos.y * rotationSpeed.y, deltaTouchPos.x*rotationSpeed.x, 0);
+        }
+    }
+    #endif
     public void Flying(bool value)
     {
         if(value == true)

af0fc042dbb50219afee09780778afc501739bb9
KitsuKitsu <akira1410.k@gmail.com>
2019-08-07 06:01:10 +0900

    [add]contentであるtoggleを動的に生成するようにした

--- Assets/VrScenes/Scripts/ColorChangePanel.cs
+++ Assets/VrScenes/Scripts/ColorChangePanel.cs
@@ -1,20 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorChangePanel : MonoBehaviour
 {
+    List<Material> contentMaterials = new List<Material>();
+    [SerializeField] public GameObject panelPref;
+
     void OnEnable()
     {
-        Debug.Log("click");
-        List<Material> contentMaterials = new List<Material>();
-        for(int i = 0; i < 10; i++)
+        Debug.Log("1");
+        for (int i = 0; i < 10; i++)
         {
             string materialName = "Color" + i.ToString();
             Material material = Resources.Load(materialName, typeof(Material)) as Material;
-            Debug.Log(material.name);
             contentMaterials.Add(material);
+            Debug.Log(contentMaterials[i].name);
         }
-        
+        Debug.Log("2");
+        Debug.Log("count: "+ contentMaterials.Count);
+        SetColorPanel();
+    }
+
+    public void SetColorPanel()
+    {
+        Debug.Log("3");
+        int materialCount = contentMaterials.Count;
+
+        //Content取得(ボタンを並べる場所)
+        GameObject canvas = GameObject.Find("Canvas");
+        RectTransform content = canvas.transform.Find("ColorChangePanel/Scroll View/Viewport/Content").gameObject.GetComponent<RectTransform>();
+
+        //Contentの高さ決定
+        float panelSpace = content.GetComponent<VerticalLayoutGroup>().spacing;      // WorldSelectButton間の高さを取得
+        float panelHeight = panelPref.GetComponent<LayoutElement>().preferredHeight;   // WorldSelectButton自体の高さを取得
+        content.sizeDelta = new Vector2(0, (panelHeight + panelSpace) * materialCount); // 上２つの要素からcontentの高さを作成
+
+        Debug.Log("4");
+        for (int i = 0; i < materialCount; i++)
+        {
+            int panelNum = i;
+
+            //ボタン生成
+            GameObject panel = (GameObject)Instantiate(panelPref);
+
+            //ボタンをContentの子に設定
+            panel.transform.SetParent(content, false);
+
+            //ボタンのテキスト変更
+            //panel.transform.GetComponentInChildren<Text>().text = WorldsData[btnNum].name;
+        }
+        Debug.Log("5");
     }
 }

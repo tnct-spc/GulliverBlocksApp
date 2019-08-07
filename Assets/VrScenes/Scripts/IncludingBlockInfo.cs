028f127511c73c281093ffd994a32fc62d344c3a
KitsuKitsu <akira1410.k@gmail.com>
2019-08-07 19:58:57 +0900

    [add]選択したときにテクスチャを変えるようにした

--- Assets/VrScenes/Scripts/IncludingBlockInfo.cs
+++ Assets/VrScenes/Scripts/IncludingBlockInfo.cs
@@ -30,8 +30,11 @@ public class IncludingBlockInfo : MonoBehaviour
         return position;
     }
 
+    public Texture texture;
+
     public void OnClickBlock()
     {
+        //Debug.Log(gameObject.GetComponent<Renderer>().material.GetTexture("_MainTex"));
         GameObject canvas = GameObject.Find("Canvas");
         GameObject panel = canvas.transform.Find("ColorChangePanel").gameObject;
 
@@ -44,17 +47,21 @@ public class IncludingBlockInfo : MonoBehaviour
         ColorChangePanel colorChangePanel = panel.GetComponent<ColorChangePanel>();
         Material material = gameObject.GetComponent<Renderer>().material;
         Color color = material.color;
+
         if (colorChangePanel.lightUpObject == null)
         {
             material.EnableKeyword("_EMISSION");
             material.SetColor("_EmissionColor", color);
+            material.SetTexture("_MainTex", texture);
             colorChangePanel.lightUpObject = gameObject;
         }
         if (gameObject.GetComponent<IncludingBlockInfo>().ID != colorChangePanel.lightUpObject.GetComponent<IncludingBlockInfo>().ID)
         {
             material.EnableKeyword("_EMISSION");
             material.SetColor("_EmissionColor", color);
+            material.SetTexture("_MainTex", texture);
             colorChangePanel.lightUpObject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+            colorChangePanel.lightUpObject.GetComponent<Renderer>().material.SetTexture("_MainTex", null) ;
             colorChangePanel.lightUpObject = gameObject;
         }
         colorChangePanel.SetupColorChangePanel(gameObject);

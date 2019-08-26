8a96ad009683ec6439aec13cc824ec8aa28f5799
IshiiKouto <takuminn717@gmail.com>
2019-08-26 11:40:15 +0900

    [update]右上の見えないボタンを10回タップすることでデバッグ画面が開くように変更した。

---
+++ Assets/Project/VrScenes/Scripts/TenTaps.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; 
+
+namespace VrScene
+{
+
+    public class TenTaps : MonoBehaviour
+    {
+        public GameObject BackToTheGame;
+        public GameObject RuntimeHierarchy;
+        public GameObject RuntimeInspector;
+
+        int TapCount = 0;
+
+
+        private void Update()
+        {
+            if (TapCount == 10)
+            {
+                this.BackToTheGame.SetActive(true);
+                this.RuntimeInspector.SetActive(true);
+                this.RuntimeHierarchy.SetActive(true);
+                TapCount = 0;
+            }
+        }
+
+        public void Tapped()
+        {
+            TapCount++;
+        }
+    }
+}

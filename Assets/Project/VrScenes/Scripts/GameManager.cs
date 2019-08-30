f294f073313c82af1bde9a769605705336a6247f
FatherScorpion <FatherScorpion@outlook.jp>
2019-08-30 09:50:24 +0900

    [update]カタカナ翻訳を修正。また、GameManager.Modeの修正処理をGameSystemで行うようにした。

--- Assets/Project/VrScenes/Scripts/GameManager.cs
+++ Assets/Project/VrScenes/Scripts/GameManager.cs
@@ -8,10 +8,6 @@ namespace VrScene
     public class GameManager : MonoBehaviour
     {
         public static string Mode = "PlayBack";
-        private void Awake()
-        {
-            if (Mode == "プレイバックモード") Mode = "PlayBack";
-        }
         public void Back_To_Title_If_Android()
         {
             if (Application.platform == RuntimePlatform.Android)

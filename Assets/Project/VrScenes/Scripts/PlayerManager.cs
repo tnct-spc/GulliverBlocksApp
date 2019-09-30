45e102e3f603d6263fb9fd840c60d0011146b133
FatherScorpion <FatherScorpion@outlook.jp>
2019-09-30 15:24:28 +0900

    [fix]デバッグログを削除した

--- Assets/Project/VrScenes/Scripts/PlayerManager.cs
+++ Assets/Project/VrScenes/Scripts/PlayerManager.cs
@@ -132,7 +132,6 @@ namespace VrScene
             isDashChecking = true;
             for(int i = 0; i < 20; i++)
             {
-                Debug.Log(i);
                 await Task.Delay(100);
                 if (isMoving == false)
                 {

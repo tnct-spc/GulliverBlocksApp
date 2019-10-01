8bd171d3b3ffc43a90a53cdfede99891a643a94d
SoutaTanaka <tanakakesouta@gmail.com>
2019-10-01 17:33:27 +0900

    hoge

--- Assets/Project/VrScenes/Scripts/Block.cs
+++ Assets/Project/VrScenes/Scripts/Block.cs
@@ -69,36 +69,52 @@ namespace VrScene
             Material material = gameObject.GetComponent<Renderer>().material;
 
 
+
             /*
                         MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
                         meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Lines, 0);
             */
             Color color = material.color;
-            Color lastColor = color;
-            color.r = Math.Abs(color.r - 0.30f);
-            color.b = Math.Abs(color.b - 0.30f);
-            color.g = Math.Abs(color.g - 0.30f);
+            Material lastMaterial = material;
+            //color.r = Math.Abs(color.r - 0.30f);
+            //color.b = Math.Abs(color.b - 0.30f);
+            //color.g = Math.Abs(color.g - 0.30f);
+            color.a = color.a * 0.3F;
             material.SetColor("_Color", color);
             
             if (colorChangePanel.lightUpObject == null)
             {
-             //   material.EnableKeyword("_EMISSION");
-              //  material.SetColor("_EmissionColor", lastColor);
+                //   material.EnableKeyword("_EMISSION");
+                //  material.SetColor("_EmissionColor", lastColor);
+
+
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                material.SetInt("_ZWrite", 0);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.EnableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                material.renderQueue = 3000;
+
+
                 material.SetColor("_Color", color);
-                material.SetTexture("_MainTex", texture);
+                //material.SetTexture("_MainTex", texture);
                 colorChangePanel.lightUpObject = gameObject;
-                colorChangePanel.lastBlockColor = lastColor;
             }
             if (gameObject.GetComponent<Block>().ID != colorChangePanel.lightUpObject.GetComponent<Block>().ID)
             {
-               // material.EnableKeyword("_EMISSION");
-               // material.SetColor("_EmissionColor", lastColor);
+                // material.EnableKeyword("_EMISSION");
+                // material.SetColor("_EmissionColor", lastColor);
+
+
+                material = lastMaterial;
+
                 material.SetTexture("_MainTex", texture);
                 material.SetColor("_Color", color);
-                colorChangePanel.lightUpObject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+                //colorChangePanel.lightUpObject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
                 colorChangePanel.lightUpObject.GetComponent<Renderer>().material.SetColor("_Color", colorChangePanel.lastBlockColor);
-                colorChangePanel.lightUpObject.GetComponent<Renderer>().material.SetTexture("_MainTex", null);
-                colorChangePanel.lastBlockColor = lastColor;
+                //colorChangePanel.lightUpObject.GetComponent<Renderer>().material.SetTexture("_MainTex", null);
 
                 colorChangePanel.lightUpObject = gameObject;
             }
